eef3193b2bfaf868653e954a066d3648592b6e46
KingBrink <kyle.brink1602@gmail.com>
2024-05-14 15:32:36 +0200

    yeah

---
+++ script/code.js
@@ -0,0 +1,10 @@
+let friends = []
+let addFriends = document.querySelector('[addFriends')
+addFriends.addEventListener('click', () => {
+    friends.push({
+        firstName: document.querySelector('[firstName]').value,
+        lastName: document.querySelector('[lastName]').value
+ })
+ console.table(friends);
+ console.log(frineds);
+})
